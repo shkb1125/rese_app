bd111c37a932d9324332064c2940ed319f4957c7
a.kanno <shkb1125@gmail.com>
2024-08-11 01:47:40 +0900

    add:LoginControllerの作成とログイン機能の実装、ログイン後店舗一覧画面への遷移

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,19 +2,20 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/', [ShopController::class, 'index']);
-//     Route::get('/mypage', [UserController::class, 'mypage']);
-//     Route::get('/detail/:shop_id', [ShopController::class, 'detail'];
-//     Route::post('/done', [ShopController::class, 'done']);
-// });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [ShopController::class, 'index']);
+    // Route::get('/mypage', [UserController::class, 'mypage']);
+    // Route::get('/detail/:shop_id', [ShopController::class, 'detail'];
+    // Route::post('/done', [ShopController::class, 'done']);
+});
 
 // 会員登録・ログイン機能
 Route::get('/register', [RegisterController::class, 'create']);
 Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-// Route::post('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login']);
 // Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/thanks', [RegisterController::class, 'thanks'])->name('thanks');
