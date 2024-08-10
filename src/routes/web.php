61e230ce4e68532dfc3739223dc73b79e5fa3933
a.kanno <shkb1125@gmail.com>
2024-08-11 01:41:28 +0900

    refactor:web.phpの不要な記述削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,30 +4,12 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
 // Route::middleware(['auth'])->group(function () {
 //     Route::get('/', [ShopController::class, 'index']);
 //     Route::get('/mypage', [UserController::class, 'mypage']);
 //     Route::get('/detail/:shop_id', [ShopController::class, 'detail'];
 //     Route::post('/done', [ShopController::class, 'done']);
 // });
-
 
 // 会員登録・ログイン機能
 Route::get('/register', [RegisterController::class, 'create']);
