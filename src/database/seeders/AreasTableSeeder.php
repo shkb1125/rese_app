6526cf21832c6c4f1cb4a1c18a9a70f36e978466
a.kanno <shkb1125@gmail.com>
2024-08-11 20:44:38 +0900

    add:AreasTableSeederに47都道府県を追加

---
+++ src/database/seeders/AreasTableSeeder.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AreasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '北海道',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '青森県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '岩手県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '宮城県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '秋田県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '山形県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '福島県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '茨城県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '栃木県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '群馬県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '埼玉県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '千葉県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '東京都',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '神奈川県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '新潟県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '長野県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '富山県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '石川県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '福井県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '山梨県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '愛知県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '岐阜県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '静岡県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '三重県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '滋賀県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '京都府',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '大阪府',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '奈良県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '和歌山県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '兵庫県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '鳥取県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '島根県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '岡山県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '徳島県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '香川県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '愛媛県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '広島県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '山口県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '高知県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '福岡県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '佐賀県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '長崎県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '熊本県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '大分県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '宮崎県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '鹿児島県',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'name' => '沖縄県',
+        ];
+        DB::table('areas')->insert($param);
+    }
+}
