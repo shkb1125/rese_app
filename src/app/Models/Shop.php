a130780c22a84d6ea0b29356651c0a8be8ce001d
a.kanno <shkb1125@gmail.com>
2024-08-11 21:40:01 +0900

    add:Shopモデルに各テーブルのリレーションを追記

--- src/app/Models/Shop.php
+++ src/app/Models/Shop.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    public function area()
+    {
+        return $this->belongsTo('Area');
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo('Genre');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany('reservation');
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany('favorite');
+    }
 }
